ae47d41e13763371dc98106171634f291ad4a7d9
Ciroslav47 <nemanja.cirkovic47@gmail.com>
2022-11-02 15:36:26 +0100

    refactoring code for deletePrizesBatch

--- Purge/deletePrizesBatch.js
+++ Purge/deletePrizesBatch.js
@@ -7,127 +7,76 @@ const PostPrizeEndpoint = secretValues.PostPrizeEndpoint;
 const DeletePrizeEndpoint = secretValues.DeletePrizeEndpoint;
 
 
-var prizesId = [];
+function postRequest(pagination = 0, chunkSize = 40) {
 
-async function list(number) {
-let chunkSize = 40;
-  for (let i = 0; i * chunkSize < number; i++) {
-  let Data = { "companyId": CompanyId, "perPage": chunkSize, "pageNumber": i, "sortBy": "createdAt", "order": "DESC", "searchTerm": "" };
-    await http({
-      method: 'post',
-      url: PostPrizeEndpoint,
-      data: Data,
-      headers: {
-        Authorization: Auth
-      }
-    })
-      .then((response) => {
-        response.data.data.map(el => {
-          prizesId.push(el.id);
-      
-        })
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-}
+  let Data = { "companyId": CompanyId, "perPage": chunkSize, "pageNumber": pagination, "sortBy": "createdAt", "order": "DESC", "searchTerm": "" }
 
+  return http({
+    method: 'post',
+    url: PostPrizeEndpoint,
+    data: Data,
+    headers: {
+      Authorization: Auth
+    }
+  })
+};
 
 function deleteRequest(prizeId) {
+
   return http({
     method: 'delete',
     url: `${DeletePrizeEndpoint}${prizeId}`,
     headers: {
       Authorization: Auth
     }
-  });
-}
+  })
+};
 
- function deleteAll(number) {
- list(number).then(()=>
-  {
-    const batchSize = 40;
-    var numberOfBatches =  Math.ceil(prizesId.length/batchSize);
-    console.log(numberOfBatches);
-  
-    
-  async function deleteBatch() {  
-    for (let n =0; n<numberOfBatches;n++){
+async function deleteBatch(numberOfBatches, prizesId, batchSize) {
+  for (let n = 0; n < numberOfBatches; n++) {
+    let Batch = prizesId.slice(n * batchSize, (n + 1) * batchSize);
+    let testArr = []
+    await Promise.all(Batch.map(el => { deleteRequest(el); testArr.push(el); console.log(testArr.length, "ID", el) }));
+    console.log('Batch number ', n + 1, ' deleted!');
 
-    let Batch = prizesId.slice(n*batchSize,(n+1)*batchSize);
-  
-    await Promise.all(Batch.map(el => deleteRequest(el)))
-
-    console.log('Batch number ',n+1,' deleted');
-
-    }
   }
-   deleteBatch();
-}
-)
 }
 
-deleteAll(400);
+async function list(number, chunkSize = 40) {
+  let numberOfChunks = Math.ceil(number / chunkSize);
+  console.log("AAAAAAAAAAAAAAAAAAAAAANumber of chunks ", numberOfChunks)
+  const arr = [...Array(numberOfChunks).keys()];
+  console.log("BBBBBBBBBBBBBBBB chunks to send from array", numberOfChunks)
+  let prizesId = [];
+
+
+  const responses = await Promise.all(arr.map(el => postRequest(el)));
+  const parsedResponse = (responses.map((el => el.data.data))).flat();
+  parsedResponse.map(el => prizesId.push(el.id));
+  console.log("BBBBBBBBBBBBBBB Number of prizes from responses", prizesId.length)
+  console.log("without duplicates", [... new Set(prizesId)].length);
+  return prizesId;
+}
+
+
+async function main(number) {
+  try {
+    const batchSize = 40;
+    const prizesId = await list(number)
+    const numberOfBatches = Math.ceil(prizesId.length / batchSize);
+    console.log(`Number of batches: ${numberOfBatches}\nBatch size: ${batchSize}`);
+
+
+    deleteBatch(numberOfBatches, prizesId, batchSize);
+  }
+  catch (err) {
+    console.log(err);
+  }
 
 
 
-
-// async function deleteAll(number) {
-//   const batchSize = 50;
-//   try {
-//    list(number);
-//     await list(number)
-//      var numberOfBatches = await list(number);
-//      Math.ceil(await list(number).length/batchSize)
-//   }
-//   catch (err) {
-//     console.error(err);
-//   }
-  
-  
-//    for (let n =0; (n+1)<numberOfBatches; n++)
-//   {
-//   let Batch = prizesId.slice(n*batchSize,(n+1)*batchSize);
-//   console.log(Batch)
-  
-//     for (let i =0;i<Batch.length;i++) {
-//        deleteRequest(Batch[i])
-//     }
-//   }
-  
-// }
-// list(120)
-// async function bla() {
-//   try {
-//     await list(120)
-//     console.log(prizesId)
-//   }
-//  catch (err){
-// console.error(err)
-//  }
-// }
-// bla();
+}
+list(1000)
 
 
 
-      // const batch = 80
-      // const n = 0
-      // let x = Promise.all(requests.slice(batch * n, batch * (n + 1)))
-
-
-      //   x = x.then(_ => {
-      //     n++;
-      //     list = requests.slice(batch * n, batch * (n + 1))
-      //     return Promise.all(list)
-      //   })
-
-
-
-
-
-
-      // for (let i = 0; i < response.data.data.length; i++) {
-      //   let prizeId = response.data.data[i].id;
-        
-      // }
